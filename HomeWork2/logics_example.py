c6c1a006c53d4da6dfa8ac0572325f662fb3f79d
Oleksandr <shevchenko131mail@gmail.com>
2024-01-09 01:21:59 +0100

    added lesson notes 3

---
+++ HomeWork2/logics_example.py
@@ -0,0 +1,27 @@
+new_string = 'gfjhg'
+empty_string = ''
+print(bool(new_string))
+print(bool(empty_string))
+
+new_value = True
+falce_velue = False
+print(int(new_value))
+print(int(falce_velue))
+
+a = 100
+b = 200
+print(a>b)
+print(a<b)
+print(a>=b)
+print(a<=b)
+print(a!=b)
+print(a==b)
+
+# if  це один із портів
+
+if_value_to_reed = int(input('enter your text here'))
+print(if_value_to_reed)
+
+int_value_too_reed = input('Hi dude prent int')
+if int_value_too_reed.isalpha():
+    print(int(int_value_too_reed))
