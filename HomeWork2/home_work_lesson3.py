c129c1e6fc0ab29675601079deaa4f7d48589018
Oleksandr <shevchenko131mail@gmail.com>
2024-01-10 01:04:50 +0100

    added a solution to the first problem added a solution to the second problem

--- HomeWork2/home_work_lesson3.py
+++ HomeWork2/home_work_lesson3.py
@@ -13,3 +13,17 @@ elif min >= 46 and min <= 59:
     print('Fourth quarter')
 else:
     print('Incorrect value')
+
+
+#задача 2
+birth_month = int(input('enter the month of your birthday'))
+if birth_month > 0 and birth_month <= 3:
+    print('Snow fell outside the window')
+elif birth_month >=4 and birth_month <= 6:
+    print('Everything around blossomed')
+elif birth_month >= 7 and birth_month <= 9:
+    print('The children enjoyed their summer vacation')
+elif birth_month >= 10 and birth_month <= 12:
+    print('Everything around lit up with bright colors')
+else:
+    print('Incorrect month data')
