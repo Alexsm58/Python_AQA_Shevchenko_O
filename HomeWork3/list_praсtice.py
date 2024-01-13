2d82ce1c1bd9319fb067839d3c5e50d1a7ee7e05
Oleksandr <shevchenko131mail@gmail.com>
2024-01-14 00:57:41 +0100

    added practice for third lesson

---
+++ HomeWork3/list_praсtice.py
@@ -0,0 +1,8 @@
+first_list = [1, 3, 2, 5, 4, 9, 8, 7, 6]
+#first_list.sort()
+#print(first_list)
+#print(sorted(first_list))
+#print(sorted(first_list, reverse = True))
+print(first_list[:4])
+print(first_list[4:])
+print(first_list[2:5])
