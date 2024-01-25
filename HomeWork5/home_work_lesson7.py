4c348d3b04e30926a2bf2a2f6e1b8ba17289788f
Oleksandr <shevchenko131mail@gmail.com>
2024-01-25 16:30:35 +0100

    fixed errors in task 3

--- HomeWork5/home_work_lesson7.py
+++ HomeWork5/home_work_lesson7.py
@@ -1,27 +1,23 @@
 #завдання 1
-#виводимо однакові значення в списках
+#we display the same values in the lists
 x_coordinates = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, '10']
-y_coordinates = [7, 8, 9, 10, 11, 12, 13, 14, 15]
+y_coordinates = [9, 8, 7, 10, 11, 12, 13, 14, 15]
 x_y_coordinates = list(sorted(filter(lambda x: x in x_coordinates, y_coordinates)))
 print(f'Твій список однакових єлементів {x_y_coordinates}')
 
 
 #завдання 2
-check_string_type = lambda x: isinstance(x, str)
-#перевіряємо значення в рядку на строку чи цифру
+#we check the value in the line for a period or a number
 string1 = "Hello, World!"
 string2 = 12345
+check_string_type = lambda x: isinstance(x, str)
 print(check_string_type(string1))
 print(check_string_type(string2))
 
 
 #завдання 3
-#сортування від більшого до меншого
-len_list = lambda x: [len(el) for el in x]
+#sorting from largest to smallest
 max_list = [[0, 1], [2, 3, 4, 5], [6, 7, 8, 9, 10], [1]]
-print("Довжина вкладених списків:", len_list(max_list))
+len_list = lambda x: [len(el) for el in x]
 print("Мінімальний підсписок:", min(max_list))
 print("Максимальний підсписок:", max(max_list))
-print("Сортування списку:", sorted(max_list))
-
-
