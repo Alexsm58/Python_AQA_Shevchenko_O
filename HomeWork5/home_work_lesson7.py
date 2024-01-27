7f98a4582d1a90a15a7e6675c890ea9df6a1146b
Alexsm58 <shevchenko131sm@gmail.com>
2024-01-27 20:19:02 +0100

    corrected the text in the print

--- HomeWork5/home_work_lesson7.py
+++ HomeWork5/home_work_lesson7.py
@@ -3,7 +3,7 @@
 x_coordinates = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, '10']
 y_coordinates = [9, 8, 7, 10, 11, 12, 13, 14, 15]
 x_y_coordinates = list(sorted(filter(lambda x: x in x_coordinates, y_coordinates)))
-print(f'Твій список однакових єлементів {x_y_coordinates}')
+print(f'Your list of identical elements {x_y_coordinates}')
 
 
 #завдання 2
@@ -19,5 +19,5 @@ print(check_string_type(string2))
 #sorting from largest to smallest
 max_list = [[0, 1], [2, 3, 4, 5], [6, 7, 8, 9, 10], [1]]
 len_list = lambda x: [len(el) for el in x]
-print("Мінімальний підсписок:", min(max_list))
-print("Максимальний підсписок:", max(max_list))
+print("Minimal sublist:", min(max_list))
+print("Maximum sublist:", max(max_list))
