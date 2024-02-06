ac60033b56014dbb3d64da1fdaf2f9bd96fcb643
Oleksandr <shevchenko131mail@gmail.com>
2024-02-06 17:01:22 +0100

    added files for task 2

--- HomeWork8/get_domain_names/file_get_domain_names.py
+++ HomeWork8/get_domain_names/file_get_domain_names.py
@@ -5,6 +5,7 @@ def domains():
     domains_file = open('domains.txt', 'r')
     domain_name_read = domains_file.readlines()
     print(domain_name_read)
+    domains_file.close()
     pattern = [re.sub(r'[.\n]', '', items) for items in domain_name_read]   #remove the dots
     for item in pattern:    #displayed in a column
         text = item.split()
