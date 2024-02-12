510c5d6502f26b899a9834e33747bb21bb1dfdd6
Oleksandr <shevchenko131mail@gmail.com>
2024-02-12 20:38:39 +0100

    the first attempt to do task 2

--- HomeWork9/home_work_lesson12.py
+++ HomeWork9/home_work_lesson12.py
@@ -1,8 +1,51 @@
+import datetime as dt
+
+#завдання 1
+def add_day_func():
+    year_date = dt.datetime(2023, 5, 3,14,5,6)
+    and_date = dt.timedelta(weeks=7, days=10, seconds=4)
+    delta_date = dt.timedelta(days=3)
+    result_year_delta = year_date - delta_date
+    result_day_delta = and_date - delta_date
+    print(result_year_delta)
+    print(result_day_delta)
+    if result_year_delta < 0 or result_day_delta < 0:
+        print(True)
+    else:
+        print(False)
+add_day_func()
+
+'''def add_day_func():
+    year_date = dt.datetime(2023, 5, 3)
+    and_date = dt.timedelta(weeks=7, days=10)
+    delta_date = dt.timedelta(days=3)
+    result_year_delta = year_date - delta_date
+    result_day_delta = and_date - delta_date
+    print(result_year_delta)
+    print(result_day_delta)
+
+add_day_func()'''
 
 
+'''    if i in result_day_delta >= 0:
+        print(True)
+    else:
+        print(False)'''
+
+#завдання 2
+'''def birthday_day():
+    print(dt.date(year=2011,month=1,day=23))
+    #print(dt.datetime.now())
+    today = dt.datetime.now()
+    my_birthday = dt.datetime(year=1989,month=6,day=1)
+    print(today)
+    print(my_birthday)
+    return today - my_birthday
+birthday_day()'''
 
 #завдання 3
 def sum_func(a, b):
+    #processing 'FileNotFoundError' exception
     try:
         with open('file.txt') as file_test:
             line_red = file_test.readlines()
