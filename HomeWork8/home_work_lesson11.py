1e7b5b420d11605c8b5c2e54c9512b773dde837c
Oleksandr <shevchenko131mail@gmail.com>
2024-02-19 20:30:56 +0100

    added fix for task2 in homework 8 lesson 11

--- HomeWork8/home_work_lesson11.py
+++ HomeWork8/home_work_lesson11.py
@@ -1,15 +1,6 @@
 import random
 
 #завдання 1
-def list_100():
-    # Use list comprehension to create a list that
-    list_new = [random.randint(0, 99) for _ in range(100) if len(str(random.randint(0, 99))) < 3]
-    print(list_new)
-
-list_100()
-
-
-#завдання 2
 def show_func(func):
     #calling a function using a decorator
     def inner(*args, **kwargs):    #created a decorator
@@ -34,3 +25,16 @@ result_sum = add_sum(x, y)
 result_multiplication = add_multiplication(x, y)
 print(f'Your sum is: ', result_sum)
 print(f'Your multiplication is: ', result_multiplication)
+
+
+#завдання 2
+def list_100():
+    #сreating a list of 100 items, each in the range 0 to 10
+    random_numbers = [random.randint(0, 10) for _ in range(100)]
+
+    #сounting the number of each element
+    count_dict = {index: count for index, count in enumerate(random_numbers)}
+    for index, count in count_dict.items():
+        print(f"{index} - {count} pieces")
+
+list_100()
