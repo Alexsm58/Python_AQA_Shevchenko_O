57a2d8c27eda87dd96da6e38835db23102676165
Oleksandr <shevchenko131mail@gmail.com>
2024-02-27 13:03:36 +0100

    added fix comments

--- HomeWork11/home_work_lesspn14.py
+++ HomeWork11/home_work_lesspn14.py
@@ -23,7 +23,6 @@ class Museum(ABC):    #add abstract class
         self.__status = options
 
 
-
 class PaintingsArt(Museum):    #inheritance
     def __init__(self, creation_year, assessed_value, room):
         super().__init__(creation_year, assessed_value, room)
--- HomeWork12/home_work_lesson15.py
+++ HomeWork12/home_work_lesson15.py
@@ -1,15 +1,16 @@
 class TrainCar:
+    #we count the cars in the train, the people in the cars, the length of the car and the train
     def __init__(self, passenger_name, destination, place=None):
         self.passenger_name = passenger_name
         self.destination = destination
         self.place = place
-        self.passengers = dict()  # Создаем пустой словарь для хранения вагонов
+        self.passengers = dict()    #create dictionary to store passengers in carriages
         self.class_train = 'First'
 
-    def __add__(self, other):
+    def __add__(self, other):   #add wagon instances
         return Train(self.class_train, other.second_class_train)
 
-    def __radd__(self, other):
+    def __radd__(self, other):   #add reverse instance of cars
         return Train(self.class_train, other.second_class_train)
 
     @property
@@ -22,12 +23,13 @@ class TrainCar:
 
 
 class TrainCar2:
+    #create the first carriage
     def __init__(self, passenger_name, destination, place=None):
         self.second_class_train = 'Second'
         self.passenger_name = passenger_name
         self.destination = destination
         self.place = place
-        self.passengers = dict()  # Создаем пустой словарь для хранения пассажиров
+        self.passengers = dict()    #create dictionary to store passengers in carriages
 
     @property
     def passenger_name(self):
@@ -37,11 +39,13 @@ class TrainCar2:
     def passenger_name(self, name):
         self._passenger_name = name
 
+
 class Train:
+    #create the second carriage
     def __init__(self, class_train, second_class_train):
         self.class_train = class_train
         self.second_class_train = second_class_train
-        self.stages = dict()
+        self.stages = dict()    #create an empty dictionary for storing cars on a train
 
     def __setitem__(self, key, value):
         self.stages[key] = value
@@ -62,26 +66,27 @@ first_vagon = Train('lux', 'polulux')
 
 vagon = TrainCar('Ivonna', '160', 66)
 vagon1 = TrainCar('Igor', '180', 99)
+
 first_vagon[vagon.passenger_name] = vagon
 first_vagon[vagon1.passenger_name] = vagon1
 
 vagon2 = TrainCar2('Ivon', '260', 88)
 train_car2.passengers[vagon2.passenger_name] = vagon2
 
-print(f'\tКоличество вагонов: {len(first_vagon)}')
+print(f'\tNumber of cars: {len(first_vagon)}')
 
-# Вывод пассажиров и их мест
-print("\tВагон: 1")
+#displaying passengers and their seats
+print("\tRailway carriage: 1")
 for passenger, car in first_vagon.stages.items():
-    print(f"\tПассажир: {passenger},\n\tМесто: {car.place}")
+    print(f"\tPassenger: {passenger},\n\tPlace: {car.place}")
 
-# Выводим количество пассажиров в первом вагоне
-print(f'\tКоличество пассажиров в первом вагоне: {len(first_vagon)}')
+#we display the number of passengers in the first carriage
+print(f'\tNumber of passengers in the first carriage: {len(first_vagon)}')
 
-# Вывод пассажиров и их мест второго вагона
-print("\tВагон: 2")
+#removal of passengers and their seats from the second carriage
+print("\tRailway carriage: 2")
 for passenger, car in train_car2.passengers.items():
-    print(f"\tПассажир: {passenger},\n\tМесто: {car.place}")
+    print(f"\tPassenger: {passenger},\n\tPlace: {car.place}")
 
-# Выводим количество пассажиров во втором вагоне
-print(f'\tКоличество пассажиров во втором вагоне: {len(train_car2.passengers)}')
+#we display the number of passengers in the second carriage
+print(f'\tNumber of passengers in the first carriage: {len(train_car2.passengers)}')
