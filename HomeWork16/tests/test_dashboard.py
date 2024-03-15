f43015291ec750590f2aa2b4945d2e6c99032ded
Oleksandr <shevchenko131mail@gmail.com>
2024-03-15 23:10:46 +0100

    added test fix

--- HomeWork16/tests/test_dashboard.py
+++ HomeWork16/tests/test_dashboard.py
@@ -2,3 +2,7 @@
 def test_go_to_sale(dashboard, driver):
     dashboard.click_sale()
     assert dashboard.driver.title == 'Зоомагазин Petslike.net - Интернет-Магазин Зоотоваров Для Ваших Питомцев'
+
+def test_to_discount(dashboard, driver):
+    dashboard.click_header_discount()
+    assert dashboard.driver.title == 'дисконтна программа | зоомагазин, Україна'
